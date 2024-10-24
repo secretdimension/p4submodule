8a51e7d4294b8748b3fd53084fea3cc017091fe5
Colden Cullen <ccullen@secretdimension.com>
2024-10-24 14:26:17 -0700

    Finish implementing `create` command
    Includes git cloning with ssh key support.

--- p4_git_submodule/cli.py
+++ p4_git_submodule/cli.py
@@ -1,10 +1,44 @@
-import click
+import re
+from os.path import expanduser
 from pathlib import Path
-from urllib.parse import urlparse
+from typing import Optional
+from urllib.parse import urlparse, urlunparse
+
+import click
+import pygit2
+from paramiko.config import SSHConfig
 
 from .config_file import ConfigFile
 
+# Replace git@github.com:org/repo.git with ssh://git@github.com/org/repo.git
+GIT_SSH_REGEX = re.compile(R"([\w\.]+)@([\w\.]+):([\w\.@\:/\-~]+)")
+
+class MyRemoteCallbacks(pygit2.RemoteCallbacks):
+
+    def credentials(self, url_str, username_from_url, allowed_types):
+        url = urlparse(url_str)
+
+        if allowed_types & pygit2.enums.CredentialType.USERNAME:
+            return pygit2.Username(username_from_url)
+
+        elif allowed_types & pygit2.enums.CredentialType.SSH_KEY:
+            ssh_config_path = Path(expanduser('~/.ssh/config'))
+            if not ssh_config_path.exists():
+                config = SSHConfig.from_path(ssh_config_path)
+                if url_config := config.lookup(url.hostname):
+                    if privkey := url_config.get('identityfile'):
+                        return pygit2.Keypair(username_from_url, expanduser(privkey) + ".pub", expanduser(privkey), "")
+
+            # Attempt default ssh keys
+            return pygit2.Keypair(username_from_url, expanduser("~/.ssh/id_rsa.pub"), expanduser("~/.ssh/id_rsa"), "")
+
+        else:
+            return None
+
 def config_argument(*param_decls: str):
+    """
+    Creates a click optional argument for receiving a ConfigFile parameter
+    """
     return click.argument(
         *param_decls,
         type=ConfigFile,
@@ -25,18 +59,41 @@ def dump_config(config: ConfigFile):
 @main.command()
 @config_argument('config')
 @click.option('--root', type=bool, is_flag=True)
-@click.option('--name', type=str, prompt=True)
-@click.option('--remote', type=urlparse, prompt=True)
-@click.option('--tracking', type=str, prompt=True)
+@click.option('--name', type=str)
+@click.option('--remote', type=str, prompt=True)
+@click.option('--tracking', type=str)
 @click.option('--path', type=Path)
-def create(config: ConfigFile, root: bool, name: str, remote: str, tracking: str, path: Path):
-    new = config.add_submodule(name, root)
-    new.remote = remote
-    new.tracking = tracking
+@click.option('--no-sync', type=bool, is_flag=True)
+def create(config: ConfigFile, root: bool, name: Optional[str], remote: str, tracking: Optional[str], path: Optional[Path], no_sync: bool):
+    new = config.add_submodule(name or config.path.parent.name, root)
 
-    path = path.resolve()
-    if path.is_absolute():
-        path = path.relative_to(config.path.parent)
-    new.path = path
+    if match := GIT_SSH_REGEX.match(remote):
+        remote = "ssh://{}@{}/{}".format(*match.groups())
+
+    new.remote = urlparse(remote)
+
+    if tracking:
+        new.tracking = tracking
+    elif no_sync:
+        raise click.UsageError('When --no-sync is passed, --tracking is required!')
+
+    if path:
+        path = path.resolve()
+        if path.is_absolute():
+            new.path = path = path.relative_to(config.path.parent)
+
+    if not no_sync:
+        repo = pygit2.clone_repository(
+            new.remote.geturl(),
+            new.full_path,
+            checkout_branch=tracking,
+            # depth=1, # NOTE: This breaks everything
+            callbacks=MyRemoteCallbacks())
+
+        # If user didn't specify a tracking branch, populate it from the default cloned
+        if not new.tracking:
+            new.tracking = repo.head.shorthand
+
+        new.current_ref = repo.head.resolve().target
 
     config.save()

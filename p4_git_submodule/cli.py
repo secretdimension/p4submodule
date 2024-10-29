2e0fe26e823c7078539c56e7a1cdafa2281f78fc
Colden Cullen <colden@coldencullen.com>
2024-10-28 22:49:17 -0700

    p4: Add initial P4Context support with arguments

--- p4_git_submodule/cli.py
+++ p4_git_submodule/cli.py
@@ -4,9 +4,9 @@ from typing import Optional
 from urllib.parse import urlparse
 
 import click
-import pygit2
 
 from .config_file import ConfigFile
+from .p4_context import P4Context
 
 # Replace git@github.com:org/repo.git with ssh://git@github.com/org/repo.git
 GIT_SSH_REGEX = re.compile(R"([\w\.]+)@([\w\.]+):([\w\.@\:/\-~]+)")
@@ -23,9 +23,24 @@ def config_argument(*param_decls: str):
         default='.',
     )
 
+pass_p4 = click.make_pass_decorator(P4Context, ensure=True)
+
 @click.group()
-def main():
-    pass
+@click.pass_context
+@click.option('--p4-port', type=str)
+@click.option('--p4-user', type=str)
+@click.option('--p4-client', type=str)
+def main(ctx: click.Context, p4_port: str, p4_user: str, p4_client: str):
+    p4 = P4Context()
+
+    if p4_port:
+        p4.port = p4_port
+    if p4_user:
+        p4.user = p4_user
+    if p4_client:
+        p4.client = p4_client
+
+    ctx.obj = ctx.with_resource(p4)
 
 @main.command()
 @config_argument('config')
@@ -34,13 +49,14 @@ def dump_config(config: ConfigFile):
         print(f'{module}: {vars(module)}')
 
 @main.command()
+@pass_p4
 @config_argument('config')
 @click.option('--name', type=str)
 @click.option('--remote', type=str, prompt=True)
 @click.option('--tracking', type=str)
 @click.option('--path', type=Path)
 @click.option('--no-sync', type=bool, is_flag=True)
-def create(config: ConfigFile, name: Optional[str], remote: str, tracking: Optional[str], path: Optional[Path], no_sync: bool):
+def create(p4: P4Context, config: ConfigFile, name: Optional[str], remote: str, tracking: Optional[str], path: Optional[Path], no_sync: bool):
     new = config.add_submodule(name, name is None)
 
     if match := GIT_SSH_REGEX.match(remote):
@@ -64,9 +80,10 @@ def create(config: ConfigFile, name: Optional[str], remote: str, tracking: Optio
     config.save()
 
 @main.command()
+@pass_p4
 @config_argument('config')
 @click.option('-m', '--message', type=str)
-def update(config: ConfigFile, message: Optional[str]):
+def update(p4: P4Context, config: ConfigFile, message: Optional[str]):
     for module in config.submodules:
         module.update(commit_message=message)
     config.save()
---
+++ p4_git_submodule/p4_context.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import os
+import socket
+from pathlib import Path
+
+import P4
+
+class P4Context(P4.P4):
+    """
+    Wrapper for P4.P4 that gives it some extra functionality that we want
+    """
+
+    def __init__(self) -> None:
+        # P4Python on Linux doesn't set cwd correctly by default, so we override it
+        super().__init__(cwd=os.getcwd())
+
+    def __enter__(self) -> P4Context:
+        # We want with statements to connect to the p4 server
+        return super().__enter__().connect()
+
+    @property
+    def client_root(self) -> str:
+        """The working directory of the currently set client"""
+
+        # This is required because if you run 'p4 clients -o <whatever>' and <whatever> is a client that doesn't exist,
+        # it will return you the template for a new client instead of erroring.
+        existing_clients = [client['client'] for client in self.run_clients('--me')]
+        if not self.client in existing_clients:
+            raise Exception("Client not set, set via: --p4-client, p4 set P4CLIENT, or .p4config")
+
+        client = self.run_client('-o', self.client)[0]
+
+        if client['Host'] != socket.gethostname():
+            raise Exception(f"Client {self.client} has a Host of {client['Host']}, but curret hostname is {socket.gethostname()}")
+
+        return Path(client['Root'])

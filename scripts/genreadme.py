c433282e4323318fbe890be5b865dc9c96eab20e
Colden Cullen <colden@coldencullen.com>
2024-11-03 22:26:39 -0800

    Add initial revision of README generator

---
+++ scripts/genreadme.py
@@ -0,0 +1,60 @@
+import os
+from importlib import import_module
+from pathlib import Path
+from typing import Optional
+
+import click
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+def _generate_commands(name: str, root_command: click.Command) -> list[click.Context]:
+    results: list[click.Context] = list()
+
+    def _recurse(name: str, child: click.Command, parent: Optional[click.Context]) -> None:
+        ctx = click.Context(child, parent=parent, info_name=name)
+        results.append(ctx)
+
+        for sub_name, sub_child in getattr(child, 'commands', {}).items():
+            _recurse(sub_name, sub_child, ctx)
+
+    _recurse(name, root_command, None)
+
+    return results
+
+def _generate_readme(template_path: str, out_path: str, commands: list[click.Context]) -> None:
+    jinja = Environment(
+        loader=FileSystemLoader([os.getcwd()]),
+        autoescape=select_autoescape(),
+    )
+
+    template = jinja.get_template(str(template_path))
+
+    stream = template.stream(commands=commands)
+
+    with click.open_file(out_path, mode="w") as readme:
+        stream.dump(readme)
+
+@click.command()
+@click.pass_context
+@click.option("--name", type=str, required=False, help="(defaults to the root of module) The name of the script to use as the base")
+@click.option("--module", type=str, required=True, prompt=True, help="The module containing the command to write")
+@click.option("--command", type=str, required=True, default="main", help="The name of the root command")
+@click.option("--template", type=click.Path(exists=True, dir_okay=False, path_type=Path), required=True, prompt=True, help="The template file to use when generating the output file")
+@click.option("--out", type=click.Path(exists=False, dir_okay=False, writable=True, path_type=Path), required=True, prompt=True, help="The path to write the output file to")
+def main(ctx: click.Context, name: Optional[str], module: str, command: str, template: Path, out: Path) -> None:
+    try:
+        module_ = import_module(module)
+    except Exception as e:
+        ctx.fail(f"Could not import module: '{module}' Error: {str(e)}")
+
+    try:
+        command_: click.Command = getattr(module_, command)
+    except:
+        ctx.fail(f"Could not find command '{command}' in module '{module}'")
+
+    name = name or module.split('.')[0]
+
+    commands = _generate_commands(name, command_)
+    _generate_readme(template, out, commands=commands)
+
+if __name__ == '__main__':
+    main()
